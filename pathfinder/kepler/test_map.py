6e23fef12956a160dce91ddd7d4c8d430f755240
Karl LaBarbara <klab@utexas.edu>
2025-01-12 16:04:37 -0500

    updating test script/map for pages

--- pathfinder/kepler/test_map.py
+++ pathfinder/kepler/test_map.py
@@ -3,7 +3,7 @@ import geopandas as gpd
 from shapely.geometry import Point
 from keplergl import KeplerGl
 
-df = pd.read_csv("../../data/path_with_identity_small.csv")
+df = pd.read_csv("../../test_data/path_with_identity_small.csv")
 
 # Create a geometry column from lat/lon; shapely uses (lon, lat) ordering
 df['geometry'] = df.apply(lambda row: Point(row['from_lon'], row['from_lat']), axis=1)
